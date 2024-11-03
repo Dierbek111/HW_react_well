bd2e2fa19fc7c7764721950cf66091a67d5ecbfd
Dierbek111 <dierbekrahmatullaev00@gmail.com>
2024-11-03 16:09:57 +0500

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <>
+        <section className="header">
+          <div className="container">
+            <div className="head__wrap">
+              <div className="header__text">
+                <h1>DIGITAL  AGENCY</h1>
+              </div>
+              <div className="header__menu">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Testimonials</a>
+                <a href="">Contact</a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        
+    </>
+  )
+}
+
+export default Header
